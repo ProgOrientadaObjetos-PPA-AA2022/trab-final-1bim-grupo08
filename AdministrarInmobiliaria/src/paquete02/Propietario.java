33afd82a99cf506425fb02bafdee78bf3a684dc7
JoseMRegalado <mjregalado2@utpl.edu.ec>
2022-05-31 20:32:32 -0500

    xd

--- AdministrarInmobiliaria/src/paquete02/Propietario.java
+++ AdministrarInmobiliaria/src/paquete02/Propietario.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete02;
-
-/**
- *
- * @author reroes
- */
-public class Propietario {
-    
+import java.io.Serializable;
+public class Propietario implements Serializable{
+    String nombre;
+    String apellido;
+    String identificacion;
+    public Propietario(String n,String a,String i){
+        nombre = n;
+        apellido = a;
+        identificacion = i;
+    }
+    public void establecerNombre(String n){
+        nombre = n;
+    }
+    public void establecerApellido(String n){
+        apellido = n;
+    }
+    public void establecerIdentificacion(String n){
+        identificacion = n;
+    }
+    public String obtenerNombre(){
+        return nombre;
+    }
+    public String obtenerApellido(){
+        return apellido;
+    }
+    public String obtenerIdentificacion(){
+        return identificacion;
+    }
+    @Override
+    public String toString(){
+        String valor = String.format("%s - %s - %s\n"
+                , obtenerNombre(), 
+                 obtenerApellido(), obtenerIdentificacion());
+        return valor;
+    }
 }
